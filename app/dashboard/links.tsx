2357156f0edac9b37145452ef8ea962c3aa1b132
ArthurBugan <arthurbugan@gmail.com>
2024-09-25 23:20:29 -0300

    feat: add help link

--- app/dashboard/links.tsx
+++ app/dashboard/links.tsx
@@ -3,7 +3,7 @@
 import { twMerge } from "tailwind-merge";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
-import { Group, PictureInPicture, Video } from "lucide-react";
+import { HelpCircle, PictureInPicture, Video } from "lucide-react";
 
 export function Links() {
   const path = usePathname();
@@ -29,6 +29,17 @@ export function Links() {
       >
         <PictureInPicture className="h-4 w-4" />
         Channels
+      </Link>
+      <Link
+        className={twMerge(
+          path.includes("channels") && "bg-gray-100 dark:bg-gray-800",
+          "flex items-center gap-3 rounded-lg px-3 py-2 text-gray-900  transition-all hover:text-gray-900 dark:text-gray-50 dark:hover:text-gray-50"
+        )}
+        target="_blank"
+        href="https://www.youtube.com/watch?v=qPte0llTKM0?utm_source=groupify_dashboard"
+      >
+        <HelpCircle className="h-4 w-4" />
+        Tutorial
       </Link>
     </>
   );
